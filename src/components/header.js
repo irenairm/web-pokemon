d79a180fd7d49784dc380c0e7592e42d2d81192f
Irena Irmalasari <13517100@std.stei.itb.ac.id>
2021-02-13 17:36:26 +0700

    add header (layout)

---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+/** @jsxImportSource @emotion/react */
+import { css, jsx } from '@emotion/react'
+import {Navbar, Nav} from 'react-bootstrap'
+import navbar from '../assets/navbar.png'
+
+function Header(){
+    return <Navbar fixed='top' bg="light" expand="lg">
+    <Navbar.Brand href="/"><img css={brand} src={navbar}></img></Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="ml-auto">
+        <Nav.Link href="/mylist">My Pokemon</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+}
+
+const brand = css `
+width: 100px;`
+export default Header;
