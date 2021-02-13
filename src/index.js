69b27d19b92f8d80386f6c61a13c61fff72885b4
Irena Irmalasari <13517100@std.stei.itb.ac.id>
2021-02-13 17:37:48 +0700

    index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// from https://www.apollographql.com/docs/tutorial/queries/
+import { ApolloClient, ApolloProvider } from "@apollo/client";
+import { cache } from "./cache";
+import React from "react";
+import ReactDOM from "react-dom";
+import Pages from "./pages";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Initialize ApolloClient
+const client = new ApolloClient({
+  cache,
+  uri: "https://graphql-pokeapi.vercel.app/api/graphql"
+});
+
+
+// Pass the ApolloClient instance to the ApolloProvider component
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
