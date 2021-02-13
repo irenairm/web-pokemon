ceb005ef929edb37ed2d91fd06566a6a72a11b6a
Irena Irmalasari <13517100@std.stei.itb.ac.id>
2021-02-13 17:37:00 +0700

    index

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as PokemonCard } from './card';
+export { default as Footer } from './footer';
+export { default as Header } from './header';
+export { default as Loading } from './loading';
