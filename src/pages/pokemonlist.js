9361c50cab76c4f65cdfa3de1a05f69ab07b1dea
Irena Irmalasari <13517100@std.stei.itb.ac.id>
2021-02-13 17:37:14 +0700

    pokemon list

---
+++ src/pages/pokemonlist.js
@@ -0,0 +1,85 @@
+/** @jsxImportSource @emotion/react */
+import { css, jsx } from '@emotion/react'
+import React, {Fragment, useState} from 'react'
+import {gql, useQuery} from '@apollo/client'
+import {PokemonCard,Loading }from '../components'
+import {Row,Col,Container} from 'react-bootstrap'
+import {withRouter} from 'react-router-dom'
+
+export const GET_POKEMON_LIST = gql`
+query pokemons($limit: Int, $offset: Int) {
+    pokemons(limit: $limit, offset: $offset) {
+      count
+      next
+      previous
+      nextOffset
+      prevOffset
+      status
+      message
+      results {
+        url
+        name
+        image
+      }
+    }
+}`;
+
+function PokemonList() {
+    const {data,loading,error,fetchMore} = useQuery(GET_POKEMON_LIST);
+    const [isLoadingMore, setIsLoadingMore] = useState(false)
+    if (loading) return <Loading />;
+    if (error) return <p>Error: {error.message}</p>;
+
+    return (<Fragment>
+      <Container>
+      <Row>
+          {
+          data.pokemons.results.map((pokemon)=>{
+              return <Col sm={3}>
+                <PokemonCard
+                img={pokemon.image} 
+                pokemon={pokemon.name} 
+                owned={1} />
+              </Col>
+            })
+          }
+      </Row>
+      {
+      data.pokemons && data.pokemons.nextOffset < data.pokemons.count && 
+      ( isLoadingMore? <p>Loading...</p> :
+        <button 
+        css ={loadButton}
+        onClick={async () => {
+          setIsLoadingMore(true);
+          await fetchMore({
+            variables: {
+              offset: data.pokemons.nextOffset
+            }
+          });
+      setIsLoadingMore(false);
+        }}>Load More</button>)
+      }
+      </Container>
+    </Fragment>
+    );
+}
+
+const loadButton = css
+`
+padding: 10px;
+    margin-top:32px;
+  margin-bottom: 16px;
+  width:fit-content;
+  font-size: 20px;
+  border-radius: 10px;
+  border: 1px solid yellow;
+  background-color: yellow;
+  color: black;
+  font-weight: bold;
+  box-shadow: 0 10px 10px rgba(0,0,0,.08), 0 0 6px rgba(0,0,0,.14);
+  &:hover {
+    cursor: pointer;
+    transform: scale(1.05);
+  }
+`
+export default withRouter(PokemonList);
